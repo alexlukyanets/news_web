d239aa21b23c61f2a4ed23ae0e47340036c12c1a
Alexandr Lukyanets <alexlukyanets18@gmail.com>
2020-11-09 11:56:06 +0200

    Create View

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from django.http import HttpResponse
 from .models import News, Category
@@ -61,15 +62,32 @@ class ViewNews(DetailView):
 #     news_item = get_object_or_404(News, pk=news_id)
 #     return render(request, 'news/view_news.html', {"news_item": news_item})
 
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
+class CreateNews(CreateView):
+    # Если у класса  News есть get_absolute_url то он делает ссылку на которую делает редирект
+    # Если нет то получим ошибку
 
-        if form.is_valid():
-            #print(form.cleaned_data)
-            #news = News.objects.create(**form.cleaned_data)
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # Можно переопределить редирект функцией reverse она занимается построением ссылки
+    # На основе именногованого адреса. Она строит ссылку передавая туда аргументы
+    # Это синоним тега URL(в html) который используется в шаблонах
+    # Только URL используется в шаблонах а reverse в коде(py)
+
+    #Функцию reverse нельзя использовать №
+    #success_url = reverse('home')
+
+    #success_url = reverse_lazy('home')
+
+
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#
+#         if form.is_valid():
+#             #print(form.cleaned_data)
+#             #news = News.objects.create(**form.cleaned_data)
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})
